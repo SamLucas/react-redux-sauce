369c01f853aab061ae4195971f1e697a754c7728
Samuel Lucas <samuellucas0603@gmail.com>
2019-12-24 12:03:16 -0300

    Finalizando com reduxsauce, estrutura completa.

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { Creators as TodoActions } from "./store/ducks/todo";
+import { Creators as TodoActions } from "./store/ducks/todos";
 
 import "./styles.css";
 
